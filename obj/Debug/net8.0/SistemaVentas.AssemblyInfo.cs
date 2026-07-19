9c258fb8c6e24c6b75753ca8b7f3c2c3fb645861
Alvin10 <alvinmanuelgonzalez88@gmail.com>
2026-07-19 14:34:25 -0400

    Ajuste a los botones del panel de login para evitar la desaparición de los botones iniciar sesión y registrarse

--- obj/Debug/net8.0/SistemaVentas.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaVentas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVentas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e26d72ebb249c826652cf2c8c59e93b9328222e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ee3fee53db0200f0c3a51eaa772c34cccab170")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVentas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVentas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
